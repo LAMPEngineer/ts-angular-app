cb94b824ed6c2f42b68605cc803b9adb5c486130
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-01-17 07:52:41 +0530

    NgFor directive

--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -10,21 +10,22 @@ import { Stock } from '../../model/stock';
 })
 export class StockItemComponent implements OnInit {
 
-	public stock!: Stock;
+	public stocks!: Array<Stock>;
 
   constructor() { }
 
   ngOnInit(): void {
 
-  	this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
-  	let diff = (this.stock.price / this.stock.previousPrice) - 1;
-  	let largeChange = Math.abs(diff) > 0.001;
-  	
+  	this.stocks = [
+  					new Stock('Test Stock Company', 'TSC', 85, 80),
+  					new Stock('Second Stock Company', 'SSC', 10, 20),
+  					new Stock('Last Stock Company', 'LSC', 876, 765)
+				];	
   }
 
-  toggleFavorite(event: any){
-  	console.log('We are toggling the favorite state for this stock', event)
-  	this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(event: any, index: any){
+  	console.log('We are toggling the favorite state for this stock', index, event);
+  	this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 
 }

142674042178964abbc05bc92584f2c0fed9da9d
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-01-20 09:19:59 +0530

    Input directive

--- stock-market/src/app/app.component.ts
+++ stock-market/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
-})
-export class AppComponent {
-  title = 'stock-market';
+ })
+
+export class AppComponent implements OnInit{
+
+  title = 'Stock Market App';
+
+  public stockObj!: Stock;
+
+   ngOnInit(): void {
+   	this.stockObj = new Stock('Test Stock Company', 'TSC', 85, 80);
+   }
 }

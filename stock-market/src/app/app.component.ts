397f7c62025617c397e8796c50948cb5e39c7ccb
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-01-21 07:50:21 +0530

    Output directive

--- stock-market/src/app/app.component.ts
+++ stock-market/src/app/app.component.ts
@@ -11,9 +11,15 @@ export class AppComponent implements OnInit{
 
   title = 'Stock Market App';
 
-  public stockObj!: Stock;
+  public stock!: Stock;
 
    ngOnInit(): void {
-   	this.stockObj = new Stock('Test Stock Company', 'TSC', 85, 80);
+   	this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
    }
+
+   onToggleFavorite(stock: Stock){
+   	console.log('Favorite for stock ', stock, ' was triggered');
+   	this.stock.favorite = !this.stock.favorite;
+   }
+
 }
--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Stock } from '../../model/stock';
 
@@ -11,12 +11,15 @@ import { Stock } from '../../model/stock';
 export class StockItemComponent {
 
   @Input() public stock!: Stock;
+  @Output() private toggleFavorite: EventEmitter<Stock>;
 
-  constructor() { }
+  constructor() { 
+  	this.toggleFavorite = new EventEmitter<Stock>();
+  }
 
   
-  toggleFavorite(event: any){
-  	this.stock.favorite = !this.stock.favorite;
+  onToggleFavorite(event: any){
+  	this.toggleFavorite.emit(this.stock);
 
   }
 

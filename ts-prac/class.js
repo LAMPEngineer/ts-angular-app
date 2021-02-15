b9d12a29150e89d3daf79b2b7f1079b932f132bf
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-02-15 10:15:18 +0530

    setter and getter

--- ts-prac/class.js
+++ ts-prac/class.js
@@ -7,7 +7,21 @@ var Point = /** @class */ (function () {
     Point.prototype.draw = function () {
         console.log('X: ' + this.x + ', Y: ' + this.y);
     };
+    // getter & setter
+    Point.prototype.getX = function () {
+        return this.x;
+    };
+    Point.prototype.setX = function (value) {
+        if (value < 0) {
+            throw new Error('value cannot be less than 0.');
+            this.x = value;
+        }
+    };
     return Point;
 }());
 var point = new Point(1, 2);
+var x = point.getX();
+point.setX(5);
+var xNew = point.getX();
+console.log('X old: ' + x + ' and X new: ' + xNew);
 point.draw();
--- ts-prac/class.ts
+++ ts-prac/class.ts
@@ -11,9 +11,29 @@ class Point{
 	}
 
 
+	// getter & setter
+	getX() {
+		return this.x;
+	}
+
+
+	setX(value) {
+		if (value < 0) {
+			throw new Error('value cannot be less than 0.');
+
+		this.x = value;			
+			
+		}
+	}
+
 }
 
 
 let point = new Point(1,2);
+
+let x = point.getX();
+point.setX(5);
+let xNew = point.getX();
+console.log('X old: ' + x + ' and X new: ' + xNew);
 
 point.draw();

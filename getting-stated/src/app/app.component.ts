6a8897fec8e6a37422b345b2e57a7158ec8082f3
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-03-13 18:17:06 +0530

    configure angular for api call

--- getting-stated/src/app/app.component.ts
+++ getting-stated/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'getting-stated';
+  books = [1, 2];
+
+  data:any = []; 
+  constructor (private http: HttpClient) {
+  	this.http.get('http://localhost/CreateRESTfulAPIs/api/itemstore/v1/items/1').subscribe(data => {
+  		this.data.push(data);
+  		console.log(this.data);
+
+  	}, error => console.error(error));
+  }
 }
--- getting-stated/src/app/app.module.ts
+++ getting-stated/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CoursesComponent } from './courses.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { CoursesService } from './courses.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
      CoursesService
